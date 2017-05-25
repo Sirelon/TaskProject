f94f3f14ec473681740a408a2f89e070b8c0f407
Alexandr Romanishyn <alexandr.romanishin@srost-studio.com>
2017-05-25 23:05:30 +0300

    prepare layout for creation task

--- app/src/main/java/com/khpi/diplom/taskproject/MainActivity.java
+++ app/src/main/java/com/khpi/diplom/taskproject/MainActivity.java
@@ -3,15 +3,10 @@ package com.khpi.diplom.taskproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -25,35 +20,21 @@ public class MainActivity extends AppCompatActivity {
         if (currentUser == null){
             openLogin();
             return;
-        } else {
-            FirebaseAuth.getInstance().signOut();
         }
 
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
-
-
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        findViewById(R.id.btn_newTask).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onClick(View v) {
+                openCreationCard();
             }
         });
+    }
+
+    private void openCreationCard() {
+        Intent createIntent = new Intent(this, CreateTaskActivity.class);
+        startActivity(createIntent);
     }
 
     private void openLogin() {
---
+++ app/src/main/java/com/khpi/diplom/taskproject/Task.java
@@ -0,0 +1,63 @@
+package com.khpi.diplom.taskproject;
+
+/**
+ * Created on 25/05/2017 22:35.
+ */
+
+public class Task {
+
+    private String id;
+    private String creatorId;
+    private String name;
+    private String description;
+    private long creationDate;
+    private String responsibleUserId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(long creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getResponsibleUserId() {
+        return responsibleUserId;
+    }
+
+    public void setResponsibleUserId(String responsibleUserId) {
+        this.responsibleUserId = responsibleUserId;
+    }
+}
